5da233b01a0d719013144b7df1ae23bc64d8a243
Zen Velez <velezrenz@gmail.com>
2022-09-13 09:56:12 +0800

    Database Setup with test integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import "./App.css";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Homepage from "./pages/homepage";
-import AboutUs from "./pages/aboutUs";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from './pages/homepage';
+import AboutUs from './pages/aboutUs';
+import Test from './pages/test';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="about" element={<AboutUs />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/test' element={<Test />} />
+				<Route path='/' element={<Homepage />} />
+				<Route path='about' element={<AboutUs />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
