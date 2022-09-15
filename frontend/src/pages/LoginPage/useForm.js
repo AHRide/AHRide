6f7c770ba712d04332b56243b04b470d3857dbf3
Blas Albert Arbuyes <blasalbert.arbuyes@sun-asterisk.com>
2022-09-15 21:38:39 +0800

    Commit for error handling fix

--- frontend/src/pages/LoginPage/useForm.js
+++ frontend/src/pages/LoginPage/useForm.js
@@ -1,33 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
-    email: "",
-    password: "",
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
   });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
     setValues({
       ...values,
-      [name]: value,
+      [name]: value
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     setErrors(validate(values));
     setIsSubmitting(true);
   };
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
-      callback();
-    }
-  });
+  useEffect(
+    () => {
+      if (Object.keys(errors).length === 0 && isSubmitting) {
+        callback();
+      }
+    },
+    [errors]
+  );
 
   return { handleChange, handleSubmit, values, errors };
 };
