ba7d8f4202c412ad8980ba31f9943567796f26a7
krishabriones-sunasterisk <108708605+krishabriones-sunasterisk@users.noreply.github.com>
2022-09-27 18:02:42 +0800

    Added DB Connections

--- frontend/src/pages/Client/ClientProfile.js
+++ frontend/src/pages/Client/ClientProfile.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import NavBarClient from "../../components/NavBarClient";
 import styles from "./ClientProfile.module.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/user.context";
 import axios from "axios";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CallIcon from "@mui/icons-material/Call";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import WcIcon from "@mui/icons-material/Wc";
 
 function ClientProfile() {
   const [userProfileList, setuserProfileList] = useState([]);
+  const { user } = useContext(UserContext);
+  console.log(user._profile.data.email);
 
   useEffect(() => {
     axios.get("http://localhost:3001/getUsers").then((response) => {
@@ -17,35 +22,65 @@ function ClientProfile() {
     });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/getUsers/${user._profile.data.email}`)
+      .then((response) => {
+        setuserProfileList(response.data);
+      });
+  }, [user._profile.data.email]);
+  const { logOutUser } = useContext(UserContext);
+
+  const logOut = async () => {
+    try {
+      // Calling the logOutUser function from the user context.
+      const loggedOut = await logOutUser();
+      // Now we will refresh the page, and the user will be logged out and
+      // redirected to the login page because of the <PrivateRoute /> component.
+      if (loggedOut) {
+        window.location.reload(true);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <NavBarClient />
       {userProfileList.map((lists, index) => (
         <div key={index}>
-          <Link
-            className={styles.backbutton}
-            style={{ textDecoration: "none" }}
-            to="/client/homepage"
-          >
-            <ArrowBackIcon fontSize="large" />
-          </Link>
-          <h1 className={styles.ProfileName}>{lists.client_name}</h1>
-          <h2 className={styles.DateFont}>{lists.client_dateCreated}</h2>
-          <button className={styles.UpdateInfoButton} type="submit">
-            Update Info
-          </button>
-          <div className={styles.Information}>
-            <h1>{lists.client_gender}</h1>
-            <LocationOnIcon fontSize="large" className={styles.Design} />
-            <h1>{lists.client_location}</h1>
-            <CallIcon fontSize="large" className={styles.Design} />
-            <h1>{lists.client_cont}</h1>
-            <AlternateEmailIcon fontSize="large" className={styles.Design} />
-            <h1>{lists.client_email}</h1>
+          <div>
+            <Link
+              className={styles.backbutton}
+              style={{ textDecoration: "none" }}
+              to="/client/homepage"
+            >
+              <ArrowBackIcon fontSize="large" />
+            </Link>
+            <h1 className={styles.ProfileName}>{lists.name}</h1>
+            <h2 className={styles.DateFont}>September 23, 2022</h2>
+            <button className={styles.UpdateInfoButton} type="submit">
+              Update Info
+            </button>
+            <div className={styles.Information}>
+              <WcIcon fontSize="large" className={styles.Design} />
+              <h1>{lists.gender}</h1>
+              <LocationOnIcon fontSize="large" className={styles.Design} />
+              <h1>New York City, USA</h1>
+              <CallIcon fontSize="large" className={styles.Design} />
+              <h1>094512345678</h1>
+              <AlternateEmailIcon fontSize="large" className={styles.Design} />
+              <h1>{lists.email}</h1>
+            </div>
+            <button
+              onClick={logOut}
+              className={styles.LogoutInfoButton}
+              type="submit"
+            >
+              logout
+            </button>
           </div>
-          <button className={styles.LogoutInfoButton} type="submit">
-            logout
-          </button>
         </div>
       ))}
     </>
--- frontend/src/pages/delivery-offers/components/DisplayOfferList.js
+++ frontend/src/pages/delivery-offers/components/DisplayOfferList.js
@@ -1,26 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "../../delivery-updates/DisplayUpdatePage.css";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 function DisplayOfferList() {
+  const navigate = useNavigate();
   const [offerList, setOfferList] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/getDeliveryOffers").then((response) => {
+    axios.get(`http://localhost:3001/getBookDelivery`).then((response) => {
       setOfferList(response.data);
     });
   }, []);
+
+  const toUpdateStatus = (_id, email) => {
+    navigate("/rider/deliveryInfo", { state: { _id, email} });
+  };
+
 
   return (
     <>
         {offerList.map((lists, index) => (
         <div className="center-layout" key={index}
        >
-          <div className="row-details" onClick={console.log(index)}>
+          <div className="row-details" onClick={() => {
+                toUpdateStatus(lists._id, lists.name)
+              }}>
           <div className="column-details"><h3>{lists.receiver_name}</h3></div>
           <div className="column-details"><h3>{lists.from}</h3></div>
+          <div className="column-details2"><h3>---</h3></div>
           <div className="column-details"><h3>{lists.to}</h3></div>
-          <div className="column-details"><h3>{lists.client_cont}</h3></div>
+          <div className="column-details"><h3>{lists.receiver_cont}</h3></div>
           </div>
         </div>
               ))}
