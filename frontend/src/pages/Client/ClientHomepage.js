591984397b25f74135826b4a1707c98d1732b662
biancanailon <ninabianca.nailon@sun-asterisk.com>
2022-09-30 20:37:49 +0800

    button in homepage working(rider/client)

--- frontend/src/pages/Client/ClientHomepage.js
+++ frontend/src/pages/Client/ClientHomepage.js
@@ -1,16 +1,18 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import NavBarClient from "../../components/NavBarClient";
 import styled from "./ClientHomepage.module.css";
+import { Link } from "react-router-dom";
 
 export default function ClientHomepage() {
-
   return (
     <>
       <NavBarClient />
       <div className={styled.Screen}>
-        <button className={styled.BookADeliveryButton} type="submit">
-          Book a Delivery
-        </button>
+        <Link to="/client/book">
+          <button className={styled.BookADeliveryButton} type="submit">
+            Book a Delivery
+          </button>
+        </Link>
       </div>
     </>
   );
