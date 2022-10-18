b7e027bc3b4ef9352bf4094e401939867504a731
biancanailon <ninabianca.nailon@sun-asterisk.com>
2022-10-18 22:43:39 +0800

    Can update profile

--- frontend/src/pages/Rider/UpdateInfo.js
+++ frontend/src/pages/Rider/UpdateInfo.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
@@ -6,10 +6,14 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import axios from "axios";
 import style from "../Rider/UpdateInfo.module.css";
+import { UserContext } from "../../contexts/user.context";
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
+  const { user } = useContext(UserContext);
+  const [updatedPost, setUpdatedPost] = React.useState({});
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,16 +22,45 @@ export default function FormDialog() {
   const handleClose = () => {
     setOpen(false);
   };
-  // const update = (post) => {
-  //   console.log(post);
-  // };
+  const [post, setPost] = React.useState({
+    name: "",
+    contact: "",
+  });
+
+  const updatePost = (post) => {
+    setUpdatedPost(post);
+    handleClickOpen();
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUpdatedPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const saveUpdatedPost = () => {
+    axios
+      .put(
+        `http://localhost:3001/UpdateInfo/rider/${user._profile.data.email}`,
+        updatedPost
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    handleClose();
+  };
 
   return (
     <div>
       <div className={style.buttonpos}>
         <Button
+          onClick={() => updatePost(post)}
           variant="outlined"
-          onClick={handleClickOpen}
+          // onClick={handleClickOpen}
           className={style.button1}
         >
           Update Info
@@ -46,6 +79,9 @@ export default function FormDialog() {
             id="name"
             label="New Name"
             type="name"
+            name="name"
+            value={updatedPost.name ? updatedPost.name : ""}
+            onChange={handleChange}
             fullWidth
             variant="standard"
           />
@@ -55,13 +91,16 @@ export default function FormDialog() {
             id="name"
             label="New Contact Number"
             type="number"
+            name="contact"
+            value={updatedPost.contact ? updatedPost.contact : ""}
+            onChange={handleChange}
             fullWidth
             variant="standard"
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button>Update</Button>
+          <Button onClick={saveUpdatedPost}>Update</Button>
           {/* onClick={() => updatePost(post)} */}
         </DialogActions>
       </Dialog>
